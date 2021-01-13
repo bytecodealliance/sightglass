1a705ec4126324f1a1518520f58d465ca3dd22d1
Andrew Brown <andrew.brown@intel.com>
2021-01-13 14:55:42 -0800

    Allow symlinks in Docker contexts of built benchmarks

--- crates/artifact/tests/benchmark.c
+++ crates/artifact/tests/benchmark.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
+#include "sightglass.h"
 
 __attribute__((export_name("add")))
-__attribute__((noinline))
-int add(int a, int b) {
+__attribute__((noinline)) int
+add(int a, int b)
+{
     return a + b;
 }
 
-__attribute__((import_module("bench")))
-__attribute__((import_name("start")))
-void bench_start();
-
-__attribute__((import_module("bench")))
-__attribute__((import_name("end")))
-void bench_end();
-
-int main() {
+int main()
+{
     bench_start();
     int c = add(40, 2);
     bench_end();
---
+++ crates/artifact/tests/sightglass.h
@@ -0,0 +1,1 @@
+../../../include/sightglass-next.h
