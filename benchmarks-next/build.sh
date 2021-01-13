130414f7872eed75ff1d9c083466851ac54a7c56
Andrew Brown <andrew.brown@intel.com>
2021-01-13 15:02:48 -0800

    Remove duplication in benchmarks-next

--- benchmarks-next/build.sh
+++ benchmarks-next/build.sh
@@ -9,10 +9,6 @@ for DOCKERFILE in $(find $PROJECT_DIR/benchmarks-next -name Dockerfile); do
     BENCH_DIR=$(dirname $DOCKERFILE)
     BENCH_NAME=$(basename $BENCH_DIR)
 
-    # Copy the sightglass header into the bench directory (TODO remove; this is a hack to avoid
-    # duplicating the header everywhere since Docker will not recognize linked context files).
-    cp $PROJECT_DIR/include/sightglass-next.h $BENCH_DIR/sightglass.h
-
     # Build the Wasm benchmark.
     $SIGHTGLASS build-benchmark $DOCKERFILE -d $BENCH_DIR/benchmark.wasm --emit-wat
 done
