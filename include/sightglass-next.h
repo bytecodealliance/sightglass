97c477ba1168a404a64aad158cdb1d0f14fa1388
Andrew Brown <andrew.brown@intel.com>
2021-01-14 16:09:08 -0800

    Add black_box

--- include/sightglass-next.h
+++ include/sightglass-next.h
@@ -19,4 +19,19 @@ __attribute__((import_module("bench")))
 __attribute__((import_name("end")))
 void bench_end();
 
+/**
+ * Call this function to prevent certain compiler-related optimizations related to knowing the value
+ * of the passed variable.
+ */
+#ifndef black_box
+static void _black_box(void *x)
+{
+    (void)x;
+}
+static void (*volatile black_box)(void *x) = _black_box;
+#else
+void black_box(void *x);
+#endif
+#define BLACK_BOX(X) black_box((void *)&(X))
+
 #endif
