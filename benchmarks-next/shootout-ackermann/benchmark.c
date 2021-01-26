354858e1f21996887f538be39797fe27903d0969
Nick Fitzgerald <fitzgen@gmail.com>
2021-01-26 13:25:57 -0800

    Make the Ackermann benchmark program read its inputs via I/O
    This avoids the possibility of a compiler optimizing away our benchmark
    computation.

--- benchmarks-next/shootout-ackermann/benchmark.c
+++ benchmarks-next/shootout-ackermann/benchmark.c
@@ -1,5 +1,8 @@
-
+#include <assert.h>
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "sightglass.h"
 
 int ackermann(int M, int N)
@@ -15,11 +18,28 @@ int ackermann(int M, int N)
     return ackermann(M - 1, ackermann(M, (N - 1)));
 }
 
+int read_int_from_file(char* path) {
+    char* buf [32] = { 0 };
+
+    int fd = open(path, 0);
+    assert(fd != -1);
+
+    ssize_t n = 0;
+    do {
+        n += read(fd, (void*) &buf, sizeof(buf) - n - 1);
+        assert(n >= 0);
+    } while (n > 0);
+    assert(close(fd) == 0);
+
+    buf[n] = '\0';
+    return atoi(&buf);
+}
+
 int main()
 {
-    int M = 3;
-    int N = 7;
-    printf("[ackermann] running with m = %d and n = %d\n", M, N);
+    int M = read_int_from_file("./default.m.input");
+    int N = read_int_from_file("./default.n.input");
+    printf("[ackermann] running with M = %d and N = %d\n", M, N);
 
     bench_start();
     int result = ackermann(M, N);
