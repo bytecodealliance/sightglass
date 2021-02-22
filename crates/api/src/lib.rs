9373044b3f786b01447e5f85042ccef6e6faa3e0
Andrew Brown <andrew.brown@intel.com>
2021-02-22 14:35:56 -0800

    Add sightglass-api
    Provides a helper crate for exposing `bench::start()` and `bench::end()` to Rust code.

---
+++ crates/api/src/lib.rs
@@ -0,0 +1,36 @@
+//! This crate provides simple bindings for Rust to the Sightglass API. The primary intent is to
+//! make it easy to instrument Rust code that will be compiled to Wasm and measured with Sightglass.
+//!
+//! For example:
+//! ```
+//! use sightglass_api as bench;
+//! bench::start();
+//! let work = 42 * 42;
+//! bench::end();
+//! ```
+//!
+//! See [benchmarks-next/README.md] for more details.
+
+mod ffi {
+    #[link(wasm_import_module = "bench")]
+    extern "C" {
+        pub fn start();
+        pub fn end();
+    }
+}
+
+/// Call this once to end sightglass recording. When compiled to Wasm, the import will look
+/// like `(import "bench" "end" (...))`.
+pub fn start() {
+    unsafe {
+        ffi::start();
+    }
+}
+
+/// Call this once to start sightglass recording. When compiled to Wasm, the import will look
+/// like `(import "bench" "start" (...))`.
+pub fn end() {
+    unsafe {
+        ffi::end();
+    }
+}
