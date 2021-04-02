7c6ba7f31dc90b60656c36be29c300b681a4862d
Andrew Brown <andrew.brown@intel.com>
2021-04-02 09:20:58 -0700

    Print CSV headers
    This change adds a field in `Format::Csv` to indicate when headers should be printed. Since headers should only be printed once in a sequence of benchmark runs, the field is internally mutable--the headers are printed or read for the first record only and never again. This seems like an internal detail so no header flag is exposed in the CLI (presumably it should be easy for users to discard the first line if headers are not desired). Fixes #107.

--- crates/data/tests/deserialize.rs
+++ crates/data/tests/deserialize.rs
@@ -9,8 +9,17 @@ fn json() {
 }
 
 #[test]
-fn csv() {
-    let file = File::open("tests/results.csv").unwrap();
-    let measurements: Vec<Measurement> = Format::Csv.read(file).unwrap();
+fn csv_no_headers() {
+    let file = File::open("tests/results-no-headers.csv").unwrap();
+    let format = Format::csv(false);
+    let measurements: Vec<Measurement> = format.read(file).unwrap();
+    assert_eq!(measurements.len(), 9);
+}
+
+#[test]
+fn csv_with_headers() {
+    let file = File::open("tests/results-with-headers.csv").unwrap();
+    let format = Format::csv(true);
+    let measurements: Vec<Measurement> = format.read(file).unwrap();
     assert_eq!(measurements.len(), 9);
 }
