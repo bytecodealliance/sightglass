86fc8a179059c97143c12596f15dc3d6b10beaff
Andrew Brown <andrew.brown@intel.com>
2023-06-13 14:31:34 -0700

    Build benchmarks into `/benchmark` directory
    All benchmarks are currently built by a `Dockerfile` located in the
    benchmark directory that emits a single `benchmark.wasm` file. This
    leads to some weirdness, like all of the `shootout` sub-benchmarks
    living in separate folders, duplicating a lot of the infrastructure
    files. This same issue will affect other multi-file benchmarks, like
    `libsodium`, which I have queued up to submit if this change is merged.
    
    What this change does is modify where the `build.sh` script expects the
    benchmark artifacts to be placed. Whereas previously the `Dockerfile`
    had to place a single file at `/benchmark.wasm`, we now can place
    multiple files in a `/benchmark` directory. All of these files will be
    extracted by `build.sh` into the benchmark directory.
    
    This commit attempts to change nothing else: only the "where to place
    the files" bit. But that apparently requires modifications to several
    `Dockerfiles` with ensuing changes to the emitted WebAssembly. The
    binary changes should not be due to any change in logic.

--- benchmarks/build.sh
+++ benchmarks/build.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-# Build a single benchmark using the `Dockerfile` in its benchmark directory. Expects the benchmark
-# directory to contain a `Dockerfile` that emits a `benchmark.wasm` file that meets the Sightglass
-# requirements (see `README.md`).
+# Build a benchmark using the `Dockerfile` in its benchmark directory. This
+# script expects the benchmark directory to contain a `Dockerfile` that creates
+# a `/benchmark` directory containing one or more `*.wasm` files that meet the
+# Sightglass requirements (see `README.md`).
 #
-# Usage: ./build.sh <path to benchmark directory> <optional filename>
+# Usage: ./build.sh <path to benchmark directory>
 
 set -e
 
@@ -14,21 +15,9 @@ if [[ ! -d $BENCHMARK_DIR ]]; then
     exit 1
 fi
 
-# If the filename is provided, use it.
-FILENAME=$2
-if [[ -z $FILENAME ]]; then
-    # Otherwise check the build dir for the default name.
-    if compgen -G "${BENCHMARK_DIR}/*.wasm" > /dev/null; then
-        FILENAME=$(ls $BENCHMARK_DIR/*.wasm  | xargs basename)
-    else
-        echo "Couldn't find the benchmark wasm filename in $BENCHMARK_DIR, please provide one."
-        exit 1
-    fi
-fi
-
 BENCHMARK_NAME=$(readlink -f $BENCHMARK_DIR | xargs basename)
 IMAGE_NAME=sightglass-benchmark-$BENCHMARK_NAME
-TMP_BENCHMARK=$(mktemp /tmp/sightglass-benchmark-XXXXXX.wasm)
+TMP_BENCHMARK=$(mktemp -d /tmp/sightglass-benchmark-XXXXXX)
 >&2 echo "Building $BENCHMARK_DIR"
 
 # Helpful logging function.
@@ -44,22 +33,25 @@ TMP_TAR=$(mktemp /tmp/sightglass-benchmark-dir-XXXXXX.tar)
 (set -x; cd $BENCHMARK_DIR && tar --create --file $TMP_TAR --dereference --verbose .)
 
 # Build the benchmark image and extract the generated `benchmark.wasm` file from its container.
-print_header "Build benchmark"
+print_header "Build benchmarks"
 (set -x; docker build --tag $IMAGE_NAME - < $TMP_TAR)
 CONTAINER_ID=$(set -x; docker create $IMAGE_NAME)
-(set -x; docker cp $CONTAINER_ID:/$FILENAME $TMP_BENCHMARK)
+(set -x; docker cp $CONTAINER_ID:/benchmark/. $TMP_BENCHMARK)
 
 # Verify benchmark is a valid Sightglass benchmark.
 print_header "Verify benchmark"
 # From https://stackoverflow.com/a/246128:
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 SIGHTGLASS_CARGO_TOML=$(dirname $SCRIPT_DIR)/Cargo.toml
-(set -x; cargo run --manifest-path $SIGHTGLASS_CARGO_TOML --quiet -- validate $TMP_BENCHMARK)
-(set -x; mv $TMP_BENCHMARK $BENCHMARK_DIR/$FILENAME)
+for WASM in $TMP_BENCHMARK/*.wasm; do
+    (set -x; cargo run --manifest-path $SIGHTGLASS_CARGO_TOML --quiet -- validate $WASM)
+    (set -x; mv $WASM $BENCHMARK_DIR/)
+done;
 
 # Clean up.
 print_header "Clean up"
 (set -x; rm $TMP_TAR)
+(set -x; rm -rf $TMP_BENCHMARK)
 (set -x; docker rm $CONTAINER_ID)
 # We do not remove the image and intermediate images here (e.g., `docker rmi $IMAGE_NAME`) to speed
 # up `build-all.sh`; use `clean.sh` instead.
