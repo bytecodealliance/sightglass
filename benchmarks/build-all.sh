be30d396b1831f2fc8a624b65634c8d9b92d2e9d
Andrew Brown <andrew.brown@intel.com>
2023-06-14 14:00:14 -0700

    Ignore `spidermonkey` rebuild with a "skip list"
    The `spidermonkey` build failures are already captured in #231.

--- benchmarks/build-all.sh
+++ benchmarks/build-all.sh
@@ -3,8 +3,9 @@
 # Either build all of the benchmarks or a random subset of them.
 #
 # Usage: ./build-all.sh <number of benchmarks>
-# - <number of benchmarks>, an optional number of benchmarks to build; if provided, this script will
-#   randomize the list of benchmarks and pick a subset of them to build
+# - <number of benchmarks>, an optional number of benchmarks to build; if
+#   provided, this script will randomize the list of benchmarks and pick a
+#   subset of them to build
 
 set -e
 # From https://stackoverflow.com/a/246128:
@@ -13,8 +14,16 @@ BENCHMARKS_DIR=$(realpath $SCRIPT_DIR)
 BUILD_SCRIPT=$BENCHMARKS_DIR/build.sh
 DOCKERFILES=$(find $BENCHMARKS_DIR -name Dockerfile)
 
-# If a numeric parameter `N` is provided to the script (e.g., `./build-all.sh 5`), randomly select
-# `N` benchmarks to rebuild; otherwise, rebuild all benchmarks.
+# Some benchmarks have known rebuild failures but are still useful to retain.
+# This "skip list" prevents them from being considered for rebuilding.
+SKIPLIST=("spidermonkey")
+for SKIP in $SKIPLIST; do
+    DOCKERFILES=$(echo $DOCKERFILES | sed -e "s+$BENCHMARKS_DIR/$SKIP/Dockerfile++")
+done
+
+# If a numeric parameter `N` is provided to the script (e.g., `./build-all.sh
+# 5`), randomly select `N` benchmarks to rebuild; otherwise, rebuild all
+# benchmarks.
 re='^[0-9]+$'
 if [[ $1 =~ $re ]]; then
     DOCKERFILES=$(echo "$DOCKERFILES" | shuf -n $1)
