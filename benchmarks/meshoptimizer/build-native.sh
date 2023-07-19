29454bf965c5cd01b0d6dea61747e30dc100240f
Johnnie Birch <johnnie.l.birch.jr@intel.com>
2023-07-18 18:41:01 -0700

    Add .gitignore and dockerfile for meshoptimizer native

--- benchmarks/meshoptimizer/build-native.sh
+++ benchmarks/meshoptimizer/build-native.sh
@@ -4,10 +4,12 @@
 #
 # Usage: ./build-native.sh
 
-(set -x; rm -r -f meshoptimizer);
-(set -x; git clone --recurse-submodules https://github.com/zeux/meshoptimizer.git);
-(set -x; cd meshoptimizer; git reset --hard f734fd572aed5bf76e84d9ed62ca6f4f6c47d84e; cd -);
-(set -x; cp sightglass.h sightglass.native.patch meshoptimizer/);
-(set -x; cd meshoptimizer; patch  -Np1 -i ./sightglass.native.patch; cd -);
-(set -x; cd meshoptimizer; CXX=/usr/bin/clang++ make codecbench-simd.so ; cd -);
+(set -x);
+(rm -r -f meshoptimizer);
+(git clone --recurse-submodules https://github.com/zeux/meshoptimizer.git);
+(cd meshoptimizer; git reset --hard f734fd572aed5bf76e84d9ed62ca6f4f6c47d84e; cd -);
+(cp sightglass.h sightglass.native.patch meshoptimizer/);
+(cd meshoptimizer; patch  -Np1 -i ./sightglass.native.patch; cd -);
+(cd meshoptimizer; CXX=/usr/bin/clang++ make codecbench-simd.so ; cd -);
+(set +x);
 
